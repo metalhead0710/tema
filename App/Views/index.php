6f7dc1391d351368daaf309bc09fa3c255302071
Mykola KOBAK <mykob@smile.fr>
2018-05-28 12:40:15 +0300

    add todo

--- App/Views/index.php
+++ App/Views/index.php
@@ -1,12 +1,15 @@
 <?php
 use App\Components\Dict as L;
+use App\Components\Url;
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?=Url::getLang()?>">
 <head>
   <title>WindroseFreight</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="content-type" content="text/html;charset=UTF-8">
+  <meta name="description" content="<!--TODO: add support-->">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
