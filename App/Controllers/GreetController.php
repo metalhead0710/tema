b9ebce516a559c73a4062c5cce7f61abbf551a44
Mykola KOBAK <mykob@smile.fr>
2018-07-02 14:23:01 +0300

    Greetings: add timer

---
+++ App/Controllers/GreetController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Components\Controller as BaseController;
+
+class GreetController extends BaseController
+{
+
+    public function index()
+    {
+        $target_date = new \DateTime('25.09.2018', new \DateTimeZone('Europe/Kiev'));
+        $now = new \DateTime('now', new \DateTimeZone('Europe/Kiev'));
+        //$now = new \DateTime('25.09.2018 23:59:59', new \DateTimeZone('Europe/Kiev'));
+
+        if ($now < $target_date) {
+            $this->view->vars(['target_date' => $target_date]);
+            return $this->view->render('timer');
+        } else if ((int)$now->format('m') === (int)$target_date->format('m') && (int)$now->format('d') === (int)$target_date->format('d')) {
+            echo "greeting calls";
+        } else {
+            echo "Error. You are too late";
+        }
+    }
+}
---
+++ assets/js/timer.js
@@ -0,0 +1,62 @@
+!(function($) {
+  'use strict';
+
+  var Greeting = {
+    options: {
+      target: null
+    },
+
+    init: function(options, root) {
+
+      this.root = $(root);
+      this.options = $.extend({}, this.options, options);
+      this.countDownDate = new Date(this.options.target);
+
+      // Bind handlers
+      this.bindHandlers();
+    },
+
+    bindHandlers: function() {
+      this.runTimer(this.countDownDate);
+    },
+    runTimer: function(countDownDate) {
+
+      setInterval(function() {
+        var now = new Date().getTime();
+
+        var distance = countDownDate - now;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24)).toString();
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) /
+            (1000 * 60 * 60)).toString();
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).
+            toString();
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000).toString();
+        if (hours.length < 2) {
+          hours = '0' + hours;
+        }
+        if (minutes.length < 2) {
+          minutes = '0' + minutes;
+        }
+        if (seconds.length < 2) {
+          seconds = '0' + seconds;
+        }
+        var color = '#' + hours + minutes + seconds;
+        document.getElementById('timer').innerHTML = days + 'днів ' + hours +
+            ':' + minutes + ':' + seconds;
+        document.body.style.background = color;
+      }, 1000);
+    }
+
+  };
+
+  App.Page.Greeting = function(options, root) {
+    root = root || $('body');
+
+    root = $(root)[0];
+    if (!$.data(root, '_Greeting')) {
+      $.data(root, '_Greeting', Object.create(Greeting).init(options, root));
+    }
+    return $.data(root, '_Greeting');
+  };
+})(jQuery);
